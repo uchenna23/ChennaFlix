c81514d25d71667cdd8517ec4302aac3fdd1df53
uchenna23 <uchennanwagbara23@gmail.com>
2024-10-16 13:06:38 -0400

    fixed imports in user controller

--- backend/src/main/java/com/example/backend/controller/UsersController.java
+++ backend/src/main/java/com/example/backend/controller/UsersController.java
@@ -3,19 +3,10 @@ package com.example.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.backend.model.Users;
 import com.example.backend.service.UsersService;
